0f1a76c71e50b596da362824d9c741ce4981bbf2
ojnode <ojinnakabu95@gmail.com>
2024-04-25 19:29:59 +0100

    Registration page and a few code refactoring.

--- quench/ContentView.swift
+++ quench/ContentView.swift
@@ -1,4 +1,3 @@
-//
 //  ContentView.swift
 //  quench
 //
@@ -8,42 +7,43 @@
 import SwiftUI
 import SwiftData
 
-
 struct ContentView: View {
     @State var email: String = ""
         
     var body: some View {
         ZStack{
-            Color.purple
+            Color.black
                 .ignoresSafeArea()
+            
             VStack (spacing: 90){
                 VStack {
                     Text("Quench")
                         .font(.system(size:70, weight: .medium))
                         .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                     Text("One day at a time")
+                        .font(Font.custom("Papyrus", size: 20))
+                        .foregroundColor(Color("mottoColor"))
                 }
                 Spacer()
+                
                 VStack {
                     TextField("Enter your username or email", text:$email)
                         .multilineTextAlignment(.center)
                         .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
                         .textFieldStyle(.roundedBorder)
                         .padding()
-                    
                     TextField("Password", text:$email)
                         .multilineTextAlignment(.center)
                         .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
                         .textFieldStyle(.roundedBorder)
                         .padding()
-                        
-                        
                     Button("Sign In") {
                         
                     }
                     .buttonStyle(AllButtonStyle())
                 }
                 Spacer()
+                
                 HStack {
                     Button("Register") {
                         
@@ -53,8 +53,6 @@ struct ContentView: View {
                 Spacer()
             }
         }
-        
-        
     }
 }
 
--- quench/Register.swift
+++ quench/Register.swift
@@ -5,4 +5,44 @@
 //  Created by prince ojinnaka on 25/04/2024.
 //
 
-import Foundation
+import SwiftUI
+import SwiftData
+
+struct RegisterView: View {
+    @State var email: String = ""
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+
+            VStack (spacing: 35) {
+                
+                Text("Quench")
+                    .font(.system(size:20, weight: .light))
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                VStack {
+                    HStack {
+                        Text("First Name")
+                            .font(.system(size:15, weight: .medium))
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        TextField("Enter your username or email", text:$email)
+                            .multilineTextAlignment(.center)
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+                        
+                    }
+                }
+                Spacer()
+            }
+ 
+        }
+
+    }
+    
+}
+
+#Preview {
+    RegisterView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
