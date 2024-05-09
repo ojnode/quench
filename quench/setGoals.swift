1aea7dbbf59012f30453031e567faa89e2eb77f9
ojnode <ojinnakabu95@gmail.com>
2024-05-09 23:35:33 +0100

    setGoals: input fields created.

--- quench/setGoals.swift
+++ quench/setGoals.swift
@@ -10,9 +10,55 @@ import SwiftUI
 
 struct SetGoal: View {
     
+    @State var age: String = ""
+    @State var weight: String = ""
+    @State var height: String = ""
+    @State var sex: String = ""
+    
     var body: some View {
         ZStack {
-            Text("checking")
+            VStack (spacing: 30) {
+                
+                HStack {
+                    Text("Age")
+                        .font(.system(size:20, weight: .medium))
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    TextField("Enter your password", text:$age)
+                        .multilineTextAlignment(.center)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                }
+                
+                HStack {
+                    Text("Weight")
+                        .font(.system(size:20, weight: .medium))
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    TextField("Enter your password", text:$weight)
+                        .multilineTextAlignment(.center)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                }
+                
+                HStack {
+                    Text("Height")
+                        .font(.system(size:20, weight: .medium))
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    TextField("Enter your password", text:$height)
+                        .multilineTextAlignment(.center)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                }
+                
+                HStack {
+                    Text("Sex")
+                        .font(.system(size:20, weight: .medium))
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    TextField("Enter your password", text:$sex)
+                        .multilineTextAlignment(.center)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                }
+            }
         }
     }
 }
