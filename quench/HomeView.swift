a8ef0122dbb77d047ad9b4e7486271d4687e086a
ojnode <ojinnakabu95@gmail.com>
2024-06-10 21:38:58 +0100

    improved UI layout and made additional views responsize

--- quench/HomeView.swift
+++ quench/HomeView.swift
@@ -10,24 +10,64 @@ import SwiftUI
 struct HomeView: View {
     
     var body: some View {
-        ZStack {
-               Stats()
-                }
+        NavigationStack {
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
                 
-                VStack (spacing:40) {
+                VStack {
+                    VStack {
+                        CreateText(label: "Quench", size: 25)
+                    }
                     
-                Spacer()
+                    VStack(spacing:25) {
+                        Stats()
+                        
+                        NavigationLink(destination: GoogleMapsView()) {
+                            VStack (spacing:25) {
+                                Image(systemName: "map.circle")
+                                    .resizable()
+                                    .frame(width: 60, height: 60)
+                                Text("Go to Map")
+                                    .foregroundColor(.black)
+                                
+                            }
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        .buttonStyle(AllButtonStyle())
+                        
+                        NavigationLink(destination: GoogleMapsView()) {
+                            VStack (spacing:25) {
+                                Image(systemName: "map.circle")
+                                    .resizable()
+                                    .frame(width: 60, height: 60)
+                                Text("Go to Map")
+                                    .foregroundColor(.black)
+                                
+                            }
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        .buttonStyle(AllButtonStyle())
+                        
+                        Spacer()
+                        
+                        Button(action: {AuthService.shared.signOut()}, label: {
+                            Text("Sign out")
+                        })
+                        .buttonStyle(AllButtonStyle())
+                    }
+                    Spacer()
                 
                     
-                    Text("socials")
-                    
-                    Button(action: {AuthService.shared.signOut()}, label: {
-                        Text("Sign out")
-                    })
                 }
+            }
         }
     }
-
+}
 
 #Preview {
     HomeView()
@@ -44,8 +84,9 @@ struct Stats: View {
                     Image(.homepage)
                         .resizable()
                         .aspectRatio(2, contentMode: .fit)
-                        .cornerRadius(25)
-                        .padding()
+                        .cornerRadius(20)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+
                 }
             }
         } else {
