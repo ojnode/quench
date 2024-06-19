39c7afc5ccbcc7bbb497157c2a17664845d78ad3
ojnode <ojinnakabu95@gmail.com>
2024-06-19 23:02:12 +0100

    created struct for retrieving firestore database and userID

--- quench/backend.swift
+++ quench/backend.swift
@@ -5,4 +5,44 @@
 //  Created by prince ojinnaka on 19/06/2024.
 //
 
-import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+enum userID: Error {
+    case IDnotfound
+}
+
+struct firebaseStoreUser {
+    let db = Firestore.firestore().collection("users")
+    let currentUser = Auth.auth().currentUser
+    
+    func getUserID() throws -> String {
+        guard let user = currentUser?.uid else {
+            throw userID.IDnotfound
+        }
+        return user
+    }
+    
+    // implement offline capablities later
+    func getData() async throws {
+        let docRef = db.document(try self.getUserID())
+        let doc = try await docRef.getDocument().data()
+        
+        
+    }
+}
+
+
+struct CalculateBMI {
+    var db = firebaseStoreUser().db
+ //   var user = try firebaseStoreUser().getUserID()
+    
+    func getUser() -> String {
+        do {
+            return try firebaseStoreUser().getUserID()
+        } catch {
+            return "something went wrong"
+        }
+    }
+
+}
