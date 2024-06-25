fcb44dddeb3cb215a8967940f53dd14a8ca7181d
ojnode <ojinnakabu95@gmail.com>
2024-06-25 02:58:20 +0100

    retrieved data from firebasestore and used to calculate the BMI.

--- quench/backend.swift
+++ quench/backend.swift
@@ -23,26 +23,84 @@ struct firebaseStoreUser {
         return user
     }
     
-    // implement offline capablities later
-    func getData() async throws {
+    func getData() async throws -> [String :Any]? {
         let docRef = db.document(try self.getUserID())
         let doc = try await docRef.getDocument().data()
-        
-        
+        return doc
     }
 }
 
-
-struct CalculateBMI {
-    var db = firebaseStoreUser().db
- //   var user = try firebaseStoreUser().getUserID()
+class CalculateBMI {
     
-    func getUser() -> String {
-        do {
-            return try firebaseStoreUser().getUserID()
-        } catch {
-            return "something went wrong"
+    let doc = firebaseStoreUser()
+    var attributesList: [String: Any] = [:]
+    var BMI: Double = 0
+    
+    func getBMI() async throws {
+        guard let attributes = try await doc.getData()
+        else {
+            throw RetrieveDataErrors.attributesError
         }
-    }
+        for (key, value) in attributes {
+            if key == "weight" || key == "height" {
+                attributesList.updateValue(value, forKey: key)
+            }
+        }
+        
+        // First, unwrap the Any type
+        guard let weightUnwrapped = attributesList["weight"] as? Any,
+              let heightUnwrapped = attributesList["height"] as? Any else {
+            throw RetrieveDataErrors.attributesError
+        }
 
+        // Then, try to convert to Double
+        let weight: Double
+        let height: Double
+
+        if let weightDouble = weightUnwrapped as? Double {
+            weight = weightDouble
+        } else if let weightString = weightUnwrapped as? String,
+                  let weightDouble = Double(weightString) {
+            weight = weightDouble
+        } else {
+            throw RetrieveDataErrors.attributesError
+        }
+        
+        if let heightDouble = heightUnwrapped as? Double {
+            height = heightDouble
+        } else if let heightString = heightUnwrapped as? String,
+                  let heightDouble = Double(heightString) {
+            height = heightDouble
+        } else {
+            throw RetrieveDataErrors.attributesError
+        }
+        
+        print("\(weight / height)")
+//        guard let weight = attributesList["weight"]  as? Double,
+//              let height = attributesList["height"]  as? Double else {
+//            throw RetrieveDataErrors.attributesError
+//        }
+
+//    print("is it working 3")
+//    print(attributesList)
+//    let weight = attributesList["weight"] as? Double
+//    let height = attributesList["height"] as? Double
+//    let BMI = weight! / height! * 2
+//    return BMI
+}
+}
+
+func printBMI() async {
+    let BMI = CalculateBMI()
+    
+    do {
+        let BMIResults = try await BMI.getBMI()
+    } catch {
+        print("e no work")
+    }
+    
+}
+
+enum RetrieveDataErrors: Error {
+    case attributesError
 }
