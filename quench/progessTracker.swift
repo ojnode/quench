7bed870040960e19c6f8338432dcc612c58b6aa3
ojnode <ojinnakabu95@gmail.com>
2024-07-18 21:01:00 +0100

    add update beside individual substance change bar colour to achieve unifrom output colour for user - red add nitof measurement to output

--- quench/progessTracker.swift
+++ quench/progessTracker.swift
@@ -9,9 +9,10 @@ import SwiftUI
 import Charts
 
 struct ProgressTracker: View {
-    
     @State var signedOut = false
+    @State var recordIntake = false
     @EnvironmentObject var BMIClass: AccessUserAttributes
+    @EnvironmentObject var units: unitCalculator
     
     var body: some View {
         ZStack {
@@ -24,6 +25,7 @@ struct ProgressTracker: View {
                     CreateText(label: "One day at a time", size: 20, design: .serif)
                 }
                 Spacer()
+                
                 VStack {
                     HStack(spacing:200) {
                         CreateImageView(image: "bmi", width: 1, height: 1,
@@ -37,12 +39,78 @@ struct ProgressTracker: View {
                 .background(Color.blue)
                 .cornerRadius(20)
                 
+                VStack {
+                    CreateText(label: "Weekly Intake", size: 25, weight: .bold, color: .black)
+                    VStack {
+                        HStack(spacing:120) {
+                            HStack(spacing:20) {
+                                CreateImageView(image: "wine", width: 1, height: 1,
+                                                radius: 10, frameWidth: 50, frameHeight: 30)
+                                CreateText(label: String(format: "%.2f unit(s)", units.totalUnits), size: 20,
+                                           weight: .semibold, color: .red)
+                            }
+                            Button(action: {recordIntake = true}, label: {
+                                Text("Update")
+                                    .frame(width: 70, height: 40)
+                                    .foregroundColor(.blue)
+                                    .background(Color.black)
+                                    .cornerRadius(10)
+                            })
+                            .navigationDestination(isPresented: $recordIntake) {
+                                AlcoholUnits().environmentObject(units)
+                            }
+                            
+                        }
+                        HStack(spacing:120) {
+                            HStack(spacing:20) {
+                                CreateImageView(image: "coffee", width: 1, height: 1,
+                                                radius: 10, frameWidth: 50, frameHeight: 30)
+                                CreateText(label: String(format: "%.2f unit(s)", 0), size: 20,
+                                           weight: .semibold, color: .red)
+                            }
+//                            Button(action: {recordIntake = true}, label: {
+//                                Text("Update")
+//                                    .frame(width: 70, height: 40)
+//                                    .foregroundColor(.blue)
+//                                    .background(Color.black)
+//                                    .cornerRadius(10)
+//                            })
+//                            .navigationDestination(isPresented: $recordIntake) {
+//                                AlcoholUnits().environmentObject(units)
+//                            }
+                            
+                        }
+                        HStack(spacing:120) {
+                            HStack(spacing:20) {
+                                CreateImageView(image: "cigarette", width: 1, height: 1,
+                                                radius: 10, frameWidth: 50, frameHeight: 30)
+                                CreateText(label: String(format: "%.2f unit(s)", 0), size: 20,
+                                           weight: .semibold, color: .red)
+                            }
+//                            Button(action: {recordIntake = true}, label: {
+//                                Text("Update")
+//                                    .frame(width: 70, height: 40)
+//                                    .foregroundColor(.blue)
+//                                    .background(Color.black)
+//                                    .cornerRadius(10)
+//                            })
+//                            .navigationDestination(isPresented: $recordIntake) {
+//                                AlcoholUnits().environmentObject(units)
+//                            }
+                            
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.blue)
+                .cornerRadius(20)
+                
                 Chart {
                     ForEach (BMIArray(userBMI:BMIClass.BMI).data) { d in
                         BarMark(x: PlottableValue.value("category", d.category),
                                 y: PlottableValue.value("range", d.range))
                         
-                        .foregroundStyle(d.userCategory == "Within Range" ? .green : Color("barColor"))
+                        .foregroundStyle(d.userCategory == "Within Range" ? .red : Color("barColor"))
                     }
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
