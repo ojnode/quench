1b8dce547724e2ca1760cb439e7a1dcb76a546c9
ojnode <ojinnakabu95@gmail.com>
2024-07-22 20:22:09 +0100

    introduce swift package manager with package.swfit file

--- Package.swift
+++ Package.swift
@@ -1,21 +1,31 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "quench",
+    platforms: [
+        .iOS(.v14) // Adjust according to your project's requirements
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "quench",
             targets: ["quench"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0") // Use the proper version
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "quench"),
+            name: "quench",
+            dependencies: [
+                "SQLite",
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseStorage", package: "firebase-ios-sdk")
+                // Add other Firebase products as needed
+            ]),
         .testTarget(
             name: "quenchTests",
             dependencies: ["quench"]),
